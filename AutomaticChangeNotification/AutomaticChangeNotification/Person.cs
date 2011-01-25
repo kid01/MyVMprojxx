bd65db0f7cb2d2e6e1db2b2d940f3165c1f3f1cb
kid01 <xiaox01@gmail.com>
2011-01-25 15:16:05 -0600

    commits8

---
+++ AutomaticChangeNotification/AutomaticChangeNotification/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutomaticChangeNotification
+{
+    /// <summary>
+    /// </summary>
+    class Person
+    {
+        private string _firstName;
+
+        public string FirstName
+        {
+            get{ return _firstName;}
+            set { _firstName=value;
+            OnFirstNameChanged();}
+
+        }
+
+        public event EventHandler FirstNameChanged;
+        private void OnFirstNameChanged()
+        {
+            if (FirstNameChanged != null)
+                FirstNameChanged(this, EventArgs.Empty);
+        }
+    }
+}
