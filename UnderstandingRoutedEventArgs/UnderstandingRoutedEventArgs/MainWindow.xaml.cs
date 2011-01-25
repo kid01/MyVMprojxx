9c928387d43a9c3d7401ffcf298b35048d2f226a
kid01 <xiaox01@gmail.com>
2011-01-25 15:16:52 -0600

    commits8

---
+++ UnderstandingRoutedEventArgs/UnderstandingRoutedEventArgs/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UnderstandingRoutedEventArgs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            object source = e.Source;
+            object originalSource = e.OriginalSource;
+            e.Handled = true;
+
+        }
+    }
+}
