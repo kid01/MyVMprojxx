5cd3a42b2aac0415b33dece1d2c5a150a9c9e13b
kid01 <xiaox01@gmail.com>
2011-01-25 15:11:32 -0600

    commits5

---
+++ GenericEventHandler/GenericEventHandler/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GenericEventHandler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button) e.Source;
+            outPut.Text = string.Format("You Choose The Color{0}!", button.Content);
+            outPut.Background = button.Foreground;
+        }
+    }
+}
