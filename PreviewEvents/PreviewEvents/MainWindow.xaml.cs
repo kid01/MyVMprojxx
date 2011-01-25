9e1ed0d7cc031d3b9cea823c2b3da14f8e2478c3
kid01 <xiaox01@gmail.com>
2011-01-25 15:13:19 -0600

    commits6

---
+++ PreviewEvents/PreviewEvents/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PreviewEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Handler(object sender, KeyEventArgs e)
+        {
+        bool isPreview = e.RoutedEvent.Name.StartsWith("Preview");
+        string direction = isPreview ? "v" : "^";
+        Output.Items.Add(string.Format("{0} {1}",direction,sender.GetType().Name));
+        if (sender == e.OriginalSource && isPreview)
+            Output.Items.Add("-{bounce}-");
+        if (sender == this && !isPreview)
+            Output.Items.Add(" -end- ");
+        }
+    }
+}
